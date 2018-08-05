b92df58694fb1ffd6a57540ddfb76806456fa91a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-05 19:01:32 +0530

    Client Controller
    Sample Client Service

--- sample-spring-cloud-netflix/sample-client-service/src/main/java/io/yadnyesh/sampleclientservice/controller/ClientController.java
+++ sample-spring-cloud-netflix/sample-client-service/src/main/java/io/yadnyesh/sampleclientservice/controller/ClientController.java
@@ -1,5 +1,16 @@
 package io.yadnyesh.sampleclientservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClientController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
 }
