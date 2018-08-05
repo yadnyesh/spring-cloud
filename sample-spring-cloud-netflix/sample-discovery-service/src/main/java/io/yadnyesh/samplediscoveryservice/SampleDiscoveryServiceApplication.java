11dca843927dcd30bf9225a972b6eac141de80a0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-05 18:09:43 +0530

    fix: Removing old code

--- sample-spring-cloud-netflix/sample-discovery-service/src/main/java/io/yadnyesh/samplediscoveryservice/SampleDiscoveryServiceApplication.java
+++ sample-spring-cloud-netflix/sample-discovery-service/src/main/java/io/yadnyesh/samplediscoveryservice/SampleDiscoveryServiceApplication.java
@@ -2,13 +2,13 @@ package io.yadnyesh.samplediscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class SampleDiscoveryServiceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SampleDiscoveryServiceApplication.class, args);
+		new SpringApplicationBuilder(SampleDiscoveryServiceApplication.class).web(true).run(args);
 	}
 }
