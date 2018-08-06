bfda69a62462853bfde62ae50443d9006e3144fc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-06 18:12:15 +0530

    Eureka Discovery Server and Client working fine

--- sample-spring-cloud-netflix/sample-client-service/src/main/java/io/yadnyesh/sampleclientservice/controller/ClientController.java
+++ sample-spring-cloud-netflix/sample-client-service/src/main/java/io/yadnyesh/sampleclientservice/controller/ClientController.java
@@ -3,8 +3,12 @@ package io.yadnyesh.sampleclientservice.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ClientController {
@@ -14,5 +18,12 @@ public class ClientController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 	
+	@GetMapping("/ping")
+	public List<ServiceInstance> ping() {
+		List<ServiceInstance> instances = discoveryClient.getInstances("CLIENT-SERVICE");
+		LOGGER.info("INSTANCES: count={}", instances.size());
+		instances.stream().forEach(it -> LOGGER.info("INSTANCE: id={}, port={}", it.getServiceId(), it.getPort()));
+		return instances;
+	}
 	
 }
